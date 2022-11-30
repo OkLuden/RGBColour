d53a9bbd5f8e5cd45b724c7f480e1ed0cac3984f
Klaudia <Klaudiadiana.k@gmail.com>
2022-11-30 20:19:54 +0000

    Completed RGBColour and Test classes. RGBColour: -now contains a createWindow method which opens a window of your RGB colour -convertToRGB method which converts user input 0-1 into RGB values -createWindow uses these RGB values to set a background color and add text displaying the RGB values Test: -contains 2 sample colour objects and some print statements -Used to create input for RGBColour.java

--- src/main/java/RGBColour/RGBColour.java
+++ src/main/java/RGBColour/RGBColour.java
@@ -1,5 +1,8 @@
 package RGBColour;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * This class initiates the red, green and blue colours
  * for the RGB color code.
@@ -7,9 +10,15 @@ package RGBColour;
  */
 
 public class RGBColour {
-    private double r = 0;
-    private double g = 0;
-    private double b = 0;
+    // User-given values 0-1
+    private double r;
+    private double g;
+    private double b;
+
+    //Values converted into RGB 0-255
+    public int rgbRed;
+    public int rgbGreen;
+    public int rgbBlue;
 
     //Constructor
 
@@ -51,6 +60,54 @@ public class RGBColour {
 
     public void setB(double b) {
         this.b = b;
+    }
+
+    // Methods
+
+    /**
+     * This method creates a JFrame window containing the user-given
+     * colour as background and the RGB value written as a label
+     */
+    public void createWindow() {
+        JFrame frame = new JFrame("RGB Colour");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(500, 400));
+
+        // Background Colour
+        frame.getContentPane().setBackground(new java.awt.Color(rgbRed, rgbGreen, rgbBlue));
+
+        // Label displaying RGB values as text
+        JLabel textLabel = new JLabel(("RGB: " + rgbRed + ", " + rgbGreen + ", " + rgbBlue), SwingConstants.CENTER);
+        textLabel.setPreferredSize(new Dimension(500, 400));
+        frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+
+        // Window Visibility
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    /**
+     * This method converts the user input double values into
+     * RGB values, making sure they are between 0 and 255
+     * @param r taking the red input value
+     * @param g taking the green input value
+     * @param b taking the blue input value
+     */
+    public void convertToRGB(double r, double g, double b) {
+        // Number to be multiplied to convert double to 1-255
+        double rgbVal = 255.0;
+
+        // Calculate individual RGB values
+        double redVal = r * rgbVal;
+        double greenVal = g * rgbVal;
+        double blueVal = b * rgbVal;
+
+        // Convert values to integers
+        rgbRed = (int)redVal;
+        rgbGreen = (int)greenVal;
+        rgbBlue = (int)blueVal;
+        System.out.println("RGB Values: R: " + rgbRed + ", G: " + rgbGreen + ", B: " + rgbBlue);
     }
 
     // String representation
--- src/main/java/RGBColour/Test.java
+++ src/main/java/RGBColour/Test.java
@@ -1,5 +1,8 @@
 package RGBColour;
 
+//import RGBColour.RGBColour.convertToRGB;
+//import static RGBColour.RGBColour.createWindow;
+
 /**
  * This class is used to test RGBColour
  * using a Test-Driven Development process.
@@ -11,7 +14,20 @@ public class Test {
 
         System.out.println("Testing RGBColour!\n");
 
-        RGBColour colour1 = new RGBColour(0.5, 0.5, 0.5);
-        System.out.println(colour1.toString());
+        // Colour 1
+        System.out.println("Colour 1:");
+        RGBColour colour1 = new RGBColour(0.5, 0.7, 0.9);
+        System.out.println(colour1);
+
+        colour1.convertToRGB(colour1.getR(), colour1.getG(), colour1.getB());
+        colour1.createWindow();
+
+        //Colour 2
+        System.out.println("\nColour 2:");
+        RGBColour colour2 = new RGBColour(0.988, 0.4745, 0.8196);
+        System.out.println(colour1);
+
+        colour2.convertToRGB(colour2.getR(), colour2.getG(), colour2.getB());
+        colour2.createWindow();
     }
 }
